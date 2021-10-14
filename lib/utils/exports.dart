79960c25d1698fdee726d1410c53085b9aa23f43
kamran khan <77343380+3kdeveloper@users.noreply.github.com>
2021-10-14 15:43:56 +0500

    organizing code structure

---
+++ lib/utils/exports.dart
@@ -0,0 +1,16 @@
+export 'package:flutter/material.dart';
+
+// Packages
+
+// Views
+
+//..... Screens
+export 'package:flutter_firebase_auth/view/screens/splash/splash_screen.dart';
+
+//..... Components
+
+// Models
+
+// Controllers
+
+// Utils
